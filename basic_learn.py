d42845af2833ce99505a804c34794cacff9700bf
cloudwalker <cloudwalkerfree@gmail.com>
2016-08-19 01:14:45 +0800

    till file

--- basic_learn.py
+++ basic_learn.py
@@ -1,6 +1,6 @@
 #coding:utf-8
-# import time
-
+import time
+import calendar 
 # x = 3
 # print type(x)
 # print x
@@ -179,25 +179,191 @@
 
 # print '__________________________________________________________'
 
-class FC(object):
-    def __init__(self, name):
-        self.name = name
+# class FC(object):
+#     def __init__(self, name):
+#         self.name = name
 
-    def greet(self, Upper = False):
-        if Upper:
-            print self.name.upper()
-        else:
-            print self.name
+#     def greet(self, Upper = False):
+#         if Upper:
+#             print self.name.upper()
+#         else:
+#             print self.name
 
-f = FC('names Jame')
-f.greet()
-f.greet(True)
+# f = FC('names Jame')
+# f.greet()
+# f.greet(True)
+# class messageMaster:
+#     'I know all'
+#     mc = 0
+
+#     def __init__(self):
+#         print 'Master\'ve been made!' 
+
+#     def MC_Count(self):
+#         print 'Master tell you: ', messageMaster.mc
+
+#     def ackNew(self):
+#         messageMaster.mc += 1
+
+#     def ackDel(self):
+#         messageMaster.mc -= 1
+
+# mes = messageMaster()
+
+# class Cdemo:
+#     '说明'
+#     Ccount = 0  
+
+#     def __init__(self, name):
+#         self.name = name
+#         Cdemo.Ccount += 1
+#         mes.ackNew()
+
+#     def __del__(self):
+#         if hasattr(self, 'name'):
+#             print self.name,
+#         print 'I\'m done'
+#         mes.ackDel()        
+
+#     def pCount(self):
+#         print 'TOTAL: %d' % Cdemo.Ccount
+
+#     def pName(self):
+#         print 'NAME: %s' % self.name
+
+# p = Cdemo('nice')
+# p.pCount()
+# p.pName()
+# print hasattr(p, 'name')
+# print hasattr(p, 'age')
+# print getattr(p, 'name')
+# print setattr(p, 'name', 'not_nice')
+# p.pName()
+# print delattr(p, 'name')
+# print setattr(p, 'ok', 'not_nice')
+# print getattr(p, 'ok')
+
+# p.name = 'reset'
+# p.age = 0
+# p.pName()
+# del p.name
+# print hasattr(p, 'name')
+# print hasattr(p, 'age')
+# print getattr(p, 'age')
+
+# q = Cdemo('again')
+
+# mes.MC_Count()
+# del p
+# mes.MC_Count()
+# del q
+# mes.MC_Count()
+
+# print '---'
+    
+# print 'doc:', Cdemo.__doc__
+# print 'name:', Cdemo.__name__
+# print 'module:', Cdemo.__module__
+# print 'bases:', Cdemo.__bases__
+# print 'dict:', Cdemo.__dict__
+
+
+# print '__________________________________________________________'
+
+
+# a, b = (1, 2), (3, 4)
+# print cmp(a, b)
+
+# c, d = (1, 'a'), (1, 'b')
+# print cmp(c, d)
+
+# e, f = (1, 'a'), (2, 'a')
+# print cmp(e, f)
+
+# # compare number first
+# g, h = (1, 'b'), (2, 'a')
+# print cmp(g, h)
+
+# # auto type change
+# i, j = (1, 1), (1.1, 1)
+# print cmp(i, j)
+
+# # number is consider the least
+# k, l = (1, 'a'), ('a', 1)
+# print cmp(k, l)
+
+# print len(k), len((1, 2, 3))
+# print max((1, 2, 3)), max((1, 2, 'a', 'b'))
+# print min((1, 2, 3)), min((1, 2, 'a', 'b'))
+
+# x = [1, 2, 3]
+# y = tuple(x)
+
+# print x, y, type(x), type(y)
+
+
+# dic = {'a': 1, 'b': 2, 'c': 3}
+# print str(dic), len(dic), type(dic)
+
+# dir = dic.copy()
+
+# dic.clear()
+# print str(dic), len(dic), type(dic)
+# print str(dir), len(dir), type(dir)
+
+# dnew = dir.fromkeys(dir)
+# print str(dnew), len(dnew), type(dnew)
+# dnew = dir.fromkeys(dir, 123)
+# print str(dnew), len(dnew), type(dnew)
+
+# print dnew.get('a', 'no such')
+# print dnew.get('d', 'no such')
+# print dnew.has_key('a'), dnew.has_key('d')
+
+# print dnew.items(), dnew.items()[0]
+# print dnew.keys(), dnew.keys()[0]
+
+# dnew.setdefault('d', 4)
+# dnew['a'] = 1
+
+# print str(dnew)
+# print str(dir)
+# dir.update(dnew)
+# print str(dir)
+
+# print dnew.values(), dnew.values()[0]
+
+# print '__________________________________________________________'
+
+# print time.strftime("%Y-%m-%d %a %A %b %B %l %y %H:%M:%S %c %j %p %U %w %Z", time.localtime())
+# print time.strftime("%x %X", time.localtime())
+# a = time.strftime("%Y-%m-%d %a %b %H:%M:%S", time.localtime())
+# print time.mktime(time.strptime(a, "%Y-%m-%d %a %b %H:%M:%S"))
+
+# b = time.clock()
+# print  calendar.month(2016, 8)
+# print time.clock() - b
+# print time.time(), time.timezone
+# print time.ctime(time.time())
+
+# print calendar.isleap(2016), calendar.monthcalendar(2016, 8)
+# print calendar.weekday(2016, 8, 28)
+
+
+
+# print '__________________________________________________________'
+
+something = raw_input()
+print something
+
+
 
 
 
 
 
 # print '__________________________________________________________'
+
 
 
 # time.sleep(15)
